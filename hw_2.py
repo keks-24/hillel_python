52be957da8e7f35c949857ea82b97a73f3d20145
kol9 <mr.keks24@gmail.com>
2021-02-07 17:46:19 +0200

    fix code after review

--- hw_2.py
+++ hw_2.py
@@ -1,33 +1,32 @@
 import random
 
+WIN_STR = 'YOU WIN!'
+LOSE_STR = 'YOU LOSE!'
+DRAW_STR = 'DRAW!'
+
+TP_USAGE_PER_DAY = 57
+QUARANTINE_LENGTH = 14
+AVG_TP_SIZE = 500
+
 def rock_scissors_paper_game(user_item):
 	"""function that emulates the game "rock, scissors, paper"""
 
-	WIN_STR = 'YOU WIN!'
-	LOSE_STR = 'YOU LOSE!'
-	DRAW_STR = 'DRAW!'
-
 	items_list = {"rock" : 2, "scissors" : 1, "paper" : 0}
-	if user_item in items_list.keys():
-		user_value = items_list[f"{user_item}"]
-		choosen_item_name, choosen_item_value = random.choices(list(items_list.items()))[0]
-		print(choosen_item_name)
-		if user_value == choosen_item_value:
-			return DRAW_STR
-		if user_value == 2 and choosen_item_value == 0:
-			return LOSE_STR
-		if user_value == 2 and choosen_item_value == 1:
-			return WIN_STR
-		if user_value == 1 and choosen_item_value == 0:
-			return WIN_STR
-		if user_value == 1 and choosen_item_value == 2:
-			return LOSE_STR
-		if user_value == 0 and choosen_item_value == 2:
-			return WIN_STR
-		if user_value == 0 and choosen_item_value == 1:
-			return LOSE_STR
-	else:
+
+	if not user_item in items_list.keys():
 		return "incorrect item"
+
+	user_value = items_list[f"{user_item}"]
+	choosen_item_name, choosen_item_value = random.choices(list(items_list.items()))[0]
+	print(choosen_item_name)
+	if user_value == choosen_item_value:
+		return DRAW_STR
+	elif (user_value == 2 and choosen_item_value == 0) or \
+			(user_value == 0 and choosen_item_value == 1) or \
+			(user_value == 1 and choosen_item_value == 2):
+		return LOSE_STR
+	else:
+		return WIN_STR
 
 
 print(rock_scissors_paper_game(input("Type: paper or scissors or rock ")))
@@ -36,10 +35,6 @@ print(rock_scissors_paper_game(input("Type: paper or scissors or rock ")))
 
 def survive_without_TP(input_data):
 	"""This function calculate enough TP for survive on quarantine or not"""
-
-	TP_USAGE_PER_DAY = 57
-	QUARANTINE_LENGTH = 14
-	AVG_TP_SIZE = 500
 
 	count_people = input_data["people"]
 	count_tp = input_data["tp"]
@@ -56,7 +51,7 @@ survive_without_TP({"people":6, "tp":10})
 def encrypt_func(input_string):
 	"""function that encrypts a given input by two steps, reverse word and replace in it letters"""
 
-	reverse_input_string = list(reversed(input_string))
+	reverse_input_string = reversed(input_string)
 	cart_dict = {"a":0, "e":1, "i":2, "o":2, "u": 3}
 	final_word = str()
 	for letter in reverse_input_string:
@@ -80,23 +75,20 @@ def tic_tac_toe(input):
 
 	for line in input:
 		for letter in line:
-			if letter == "E":
-				return "Field has empty cell"
-			else:
-				field_coordinates.append(letter)
+			field_coordinates.append(letter)
 
 	for elem in win_coordinates:
 		if field_coordinates[elem[0]] == field_coordinates[elem[1]] == field_coordinates[elem[2]]:
 			finded_winner = str(field_coordinates[elem[0]])
 			break
 
-	if finded_winner == "":
-		return "Draw"
+	if finded_winner:
+		return f"WINNER IS {finded_winner}"
+	else:
+		return "no winner"
 
-	return f"WINNER IS {finded_winner}"
 
-
-print(tic_tac_toe([["X", "O", "X"],
+print(tic_tac_toe([["X", "E", "X"],
 				   ["O", "O", "X"],
 				   ["X", "X", "O"]
 				   ]))
