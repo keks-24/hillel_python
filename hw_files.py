1d67c31a69b5ed96c629231b80eda56acc7f9220
Nick Shumkov <mr.keks24@gmail.com>
2021-02-15 17:00:57 +0200

    pre-complete task 4

--- hw_files.py
+++ hw_files.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 OS_PATH = os.getcwd()
 
@@ -87,4 +88,42 @@ def right_align_data():
 	write_file(path_file, result)
 
 
-right_align_data()
+# right_align_data()
+
+
+# ============================ TASK 4
+
+
+def count_most_finded_elem(value):
+	counter = 1
+	elem = value[0]
+	for unique_elem in set(value):  # get unique list
+		count_ = value.count(unique_elem)  # count same elements for each unique elem from set
+		if count_ > counter:
+			counter, elem = count_, unique_elem
+	return elem
+
+
+def filter_ip_file():
+	path_file = f'{OS_PATH}/files/4.txt'
+	lines = open_file(path_file)
+	filtred_ip = dict()  # dict for store result
+	for line in lines:
+		site_ip, visit_time, week_day = (line.split(' '))
+		hours_time = visit_time[0:2]
+		if site_ip in filtred_ip:  # check ip in dict if find , add info to same ip
+			filtred_ip[site_ip][0].append(hours_time)
+			filtred_ip[site_ip][1].append(week_day)
+		else:  # add first time find ip
+			filtred_ip[site_ip] = [[hours_time], [week_day]]
+	result = list()
+	max_same_time = []
+	for ip, elem in filtred_ip.items():
+		res_time, res_day = count_most_finded_elem(elem[0]), count_most_finded_elem(elem[1])
+		result.append([ip, len(elem[0]), res_day])
+		max_same_time.append(count_most_finded_elem(res_time))
+	pprint.pprint(result)
+	pprint.pprint(max_same_time)
+
+
+filter_ip_file()
