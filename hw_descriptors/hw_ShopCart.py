c12e5083dc319599fd284ecc74c7e422c92405d2
kol9 <mr.keks24@gmail.com>
2021-03-13 16:07:24 +0200

    finish hw

--- hw_descriptors/hw_ShopCart.py
+++ hw_descriptors/hw_ShopCart.py
@@ -1,30 +1,6 @@
-# Задача4
-# Необходимо создать модели работы со складскими запасами товаров и процесса оформления заказа этих товаров.
-# Cписок требований:
-# 1) Создайте товар с такими свойствами, как имя(name), подробные сведения(description or details),
-# количество на складе(quantity), доступность(availability), цена(price).
-# Добавить к этой задаче дескриптор для аттрибута цена.
-# При назначении цены товара будет автоматически добавлен НДС 20%
-# При получении цены товара, цена возврщается уже с учетом НДС
-
-# 2) Добавить товар на склад
-# 3) Удалить товар со склада
-# 4) Распечатать остаток товара по его имени
-# 5) Распечатать остаток всех товаров
-# 6) Товар может принадлежать к категории
-# 7) Распечатать список товаров с заданной категорией
-# 8) Корзина для покупок, в которой может быть много товаров с общей ценой.
-# 9) Добавить товары в корзину (вы не можете добавлять товары, если их нет в наличии)
-# 10) Распечатать элементы корзины покупок с ценой и общей суммой
-
-# 11) Оформить заказ и распечатать детали заказа по его номеру !!!!!!!!!!!!!!!!!!!
-
-# 12) Позиция заказа, созданная после оформления заказа пользователем.
-# Он будет иметь идентификатор заказа(order_id), дату покупки(date_purchased), товары(items), количество(quantity)
-# 13) После оформления заказа количество товара уменьшается на количество товаров из заказа.
 import datetime
-import time
 from pprint import pprint
+
 
 class PriceDescriptor:
 	"""descriptor that add NDS to price"""
@@ -35,7 +11,7 @@ class PriceDescriptor:
 			raise ValueError('Not valid price')
 
 	def __get__(self, instance, owner):
-		return instance._prices
+		return instance._price
 
 
 class Product:
@@ -91,6 +67,7 @@ class Storage:
 
 	def add_to_storage(self, product: Product, category):
 		"""add product to storage"""
+
 		if product.name not in self.storage.keys():
 			self.storage[product.name] = {
 				'description': product.description,
@@ -113,6 +90,7 @@ class Storage:
 
 	def get_product_quantity(self, name):
 		"""get product quantity by name"""
+
 		return f"total quantity of {name} is {self.storage[name]['quantity']}"
 
 	def get_all_products_quantity(self):
@@ -142,41 +120,52 @@ class Basket:
 	"""class that represent shop basket"""
 	order_id = 0
 	order_position = 0
+	orders_pull = {}
 
 	def __init__(self):
 		self.order_product_list = {}
 
 	def add_product_to_basket(self, product: Product):
-		if product.name not in self.order_product_list.keys():
-			if product.availability is True and product.quantity > 0:
+		if product.name not in self.order_product_list.keys():  # если товар еще не в корзине
+			if Storage.storage[product.name]['availability'] is True and Storage.storage[product.name]['quantity'] > 0:
 				self.order_product_list[product.name] = {
 					'description': product.description,
 					'price': product.price,
 					'count': 1
 				}
 			else:
-				print(f'Product not available or out of sale')
+				print(f'Product {product.name} not available or out of sale')  # если товар недостуен по каой то причине
 		else:
-			self.order_product_list[product.name]['count'] += 1
-			self.order_product_list[product.name]['price'] += round(product.price, 2)
+			self.order_product_list[product.name]['count'] += 1 # если продукт уже в корзине просто увеличиваем кол-во
 
-	def get_basket_prod_and_price(self):
+	def get_basket_prod_and_price(self):  # элементы корзины покупок с ценой и общей суммой
 		result_list = {}
+		total_sum = 0
+		_ = []
 		for key in self.order_product_list.keys():
-			result_list[key] = {
-				'price': round(self.order_product_list[key]['price'], 2)
+			total_sum += self.order_product_list[key]['price'] * self.order_product_list[key]['count']
+			_.append(key)
+			result_list = {
+				'products': _,
+				'total summ': total_sum
 			}
 
 		return result_list
 
 	def checkout(self):
+		"""finish order save order info and manage with storage quantity"""
+
 		Basket.order_id += 1
 		Basket.order_position += 1
 		quantity = 0
 		total_amount = 0
-		for key in self.order_product_list:
+
+		for key in self.order_product_list:  # считаем кол-во товаров и считаем конечную цену
 			quantity += self.order_product_list[key]['count']
-			total_amount += self.order_product_list[key]['price']
+			total_amount += self.order_product_list[key]['price'] * self.order_product_list[key]['count']
+			if key in Storage.storage.keys():
+				Storage.storage[key]['quantity'] -= self.order_product_list[key]['count']  # 13) После оформления заказа количество товара уменьшается на количество товаров из заказа.
+
 		receive = {
 			'order_id': self.order_id,
 			'date_purchased': datetime.datetime.now().strftime('%d.%m.%Y %H:%M'),
@@ -184,42 +173,88 @@ class Basket:
 			'quantity': quantity,
 			'total amount': round(total_amount, 2)
 		}
+		self.orders_pull[self.order_id] = {
+			"order position": self.order_position,  # 12) Позиция заказа, созданная после оформления заказа пользователем.
+			"order details": receive
+		}
 
 		return receive
 
+	def get_order_info_by_order_id(self, order_id):
+		if order_id in self.orders_pull.keys():
+			return self.orders_pull[order_id]
+		else:
+			print('order not found')
 
-bread = Product(name='bread', description='tasty fresh baked', availability=True)
-bread.quantity = 14
+
+# 1) Создайте товар с такими свойствами, как имя(name), подробные сведения(description or details),
+# количество на складе(quantity), доступность(availability), цена(price).
+# Добавить к этой задаче дескриптор для аттрибута цена.
+# При назначении цены товара будет автоматически добавлен НДС 20%
+# При получении цены товара, цена возврщается уже с учетом НДС
+bread = Product(name='Bread', description='tasty fresh baked', availability=True)
+bread.quantity = 6
 bread.price = 125
 
-milk = Product(name='milk', description='fresh made from farm', availability=True)
-milk.quantity = 200
+milk = Product(name='Milk', description='fresh made from farm', availability=True)
+milk.quantity = 20
 milk.price = 15.99
 
+soap = Product(name='Soap', description='clean up hands after walk', availability=True)
+soap.quantity = 10
+soap.price = 35.90
+
+
+# 2) Добавить товар на склад
 storage = Storage()
 storage.add_to_storage(bread, category='food')
 storage.add_to_storage(milk, category='food')
+storage.add_to_storage(soap, category='cleaning')
 
-print(storage)
-# print(storage.get_product('Bread'))
-# print(storage.get_product_quantity('Bread'))
-# print(storage.get_product('Milk'))
-# storage.remove_product('Bread')
-# storage.remove_product('Bread')
-# storage.get_product('Bread')
-# print(storage.get_all_products_quantity())
-# print(storage.get_product_by_category('food')
 
+# 3) Удалить товар со склада
+storage.remove_product('Soap')
+
+
+# 4) Распечатать остаток товара по его имени
+print(storage.get_product_quantity('Bread'))
+print(storage.get_product('Bread'))
+print(storage.get_product('Milk'))
+
+
+# 5) Распечатать остаток всех товаров
+print(storage.get_all_products_quantity())
+
+# 7) Распечатать список товаров с заданной категорией
+print(storage.get_product_by_category('food'))
+
+
+# 8) Корзина для покупок, в которой может быть много товаров с общей ценой.
 basket_1 = Basket()
+
+
+# 9) Добавить товары в корзину (вы не можете добавлять товары, если их нет в наличии)
 basket_1.add_product_to_basket(bread)
 basket_1.add_product_to_basket(bread)
 basket_1.add_product_to_basket(bread)
 basket_1.add_product_to_basket(bread)
 basket_1.add_product_to_basket(milk)
 
+
+# 10) Распечатать элементы корзины покупок с ценой и общей суммой
 print(basket_1.get_basket_prod_and_price())
 
+
+# 11) Оформить заказ и распечатать детали заказа по его номеру
+pprint(basket_1.checkout())
+
+
+# 11 распечатать детали заказа по его номеру
+pprint(basket_1.get_order_info_by_order_id(order_id=1))
+
+# ---------- второй заказ в котором можно проверить реакцию если раскупят товар, availability и тд
 basket_2 = Basket()
+
 basket_2.add_product_to_basket(bread)
 basket_2.add_product_to_basket(bread)
 basket_2.add_product_to_basket(milk)
@@ -227,7 +262,6 @@ basket_2.add_product_to_basket(milk)
 
 print(basket_2.get_basket_prod_and_price())
 
-pprint(basket_1.checkout())
-time.sleep(2)
 pprint(basket_2.checkout())
 
+pprint(basket_1.get_order_info_by_order_id(order_id=2))
