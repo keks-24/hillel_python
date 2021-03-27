e6e3679a16471d7954cdb3fc2dc6b1386062ad74
kol9 <mr.keks24@gmail.com>
2021-03-28 00:27:54 +0200

    fix code make async def`s

--- hw_threading.py
+++ hw_threading.py
@@ -1,9 +1,8 @@
-from aiohttp import web
-import json
-import requests
+from aiohttp import web, ClientSession
 
 
-def get_covid():
+async def get_covid():
+	session = ClientSession()
 	url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total"
 
 	querystring = {"country": "Ukraine"}
@@ -13,33 +12,38 @@ def get_covid():
 		'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com"
 	}
 
-	response = json.loads((requests.request("GET", url, headers=headers, params=querystring)).text)
+	async with session.get(url, headers=headers, params=querystring) as resp:
+		result = await resp.json()
+	await session.close()
+	return result['data']
 
-	return response['data']
 
-
-def get_joke():
+async def get_joke():
+	session = ClientSession()
 	url = "https://icanhazdadjoke.com/"
 
 	headers = {
 		'Accept': "application/json"
 	}
 
-	response = json.loads((requests.request("GET", url, headers=headers)).text)
-	return response['joke']
+	async with session.get(url, headers=headers) as resp:
+		result = await resp.json()
+	await session.close()
+	return result['joke']
 
 
-def get_fact_about_cats():
-	url = 'https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1'
-
-	response = json.loads((requests.request("GET", url)).text)
-	return response['text']
+async def get_fact_about_cats():
+	session = ClientSession()
+	async with session.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1') as resp:
+		result = await resp.json()
+	await session.close()
+	return result['text']
 
 
 async def handle(request):
-	response_covid = get_covid()
-	response_joke = get_joke()
-	response_cat_fact = get_fact_about_cats()
+	response_covid = await get_covid()
+	response_joke = await get_joke()
+	response_cat_fact = await get_fact_about_cats()
 
 	text = "Covid info: " + str(response_covid) + "\n" + "Super joke: " + str(response_joke) + '\n' + 'Fact about cat: ' + str(response_cat_fact)
 	return web.Response(text=text)
